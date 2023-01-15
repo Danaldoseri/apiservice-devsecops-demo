9ec8a6898f86ebb178cd5dd8a17be39466df418f
603-4 <603-4@bibf.com>
2023-01-15 12:55:04 +0300

    first commit

---
+++ src/main/java/com/bbk/api/apisrvice/ApisrviceApplication.java
@@ -0,0 +1,13 @@
+package com.bbk.api.apisrvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisrviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisrviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbk/api/apisrvice/ApisrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbk.api.apisrvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisrviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
